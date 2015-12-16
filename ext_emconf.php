8f901fa211c05a100ec7b710d2d4b9e5749745e3
Matthias Toscanelli <m.toscanelli@code-source.ch>
2015-12-16 02:57:44 +0100

    [TASK] Initialize extension
    Change-Id: Ic3cb841b14e620fd5b8184bb8124954328984a6d
    Reviewed-on: https://review.typo3.org/45304
    Reviewed-by: Matthias Toscanelli <m.toscanelli@code-source.ch>
    Tested-by: Matthias Toscanelli <m.toscanelli@code-source.ch>
    Reviewed-by: Code Source <info@code-source.ch>
    Tested-by: Code Source <info@code-source.ch>

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "cdsrc_template_builder".
+ *
+ * Auto generated 08-10-2015 14:45
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Template builder',
+	'description' => 'Allows to create a new "template" with some options',
+	'category' => 'be',
+	'version' => '1.0.0',
+	'state' => 'beta',
+	'uploadfolder' => false,
+	'createDirs' => '',
+	'clearcacheonload' => false,
+	'author' => 'Matthias Toscanelli',
+	'author_email' => 'm.toscanelli@code-source.ch',
+	'author_company' => 'Code-Source',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '7.5.0-7.5.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
